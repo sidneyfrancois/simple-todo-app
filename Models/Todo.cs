bbe7d24343e4e7a8df7284476c3a8d31666e9cdf
Sidney François <sidney.fvm.dev@outlook.com>
2023-03-19 22:40:46 -0300

    Add CreatedAt property

--- Models/Todo.cs
+++ Models/Todo.cs
@@ -5,6 +5,7 @@ namespace Todo.Models
         public int Id { get; set; }
         public string Title { get; set; }
         public bool Done { get; set; }
+        public DateTime CreatedAt { get; set; }
     }
 
 }
